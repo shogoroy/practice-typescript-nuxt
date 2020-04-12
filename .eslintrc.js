de4c9164877913141382b343aff27534c65b1dc8
Shogoro Yoshida <shogoro.yoshida@gmail.com>
2020-04-12 18:00:00 +0900

    :wrench: Update lint settings about trailingComma

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,10 +11,14 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended'
   ],
-  plugins: [
-    'prettier'
-  ],
+  plugins: ['prettier'],
   // add your custom rules here
   rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'none'
+      }
+    ]
   }
 }
