e9fe032d67469c1ce396c39740af0b31c3792462
Shogoro Yoshida <shogoro.yoshida@gmail.com>
2020-04-12 18:00:35 +0900

    :sparkles: Implement calender

--- pages/calender/index.vue
+++ pages/calender/index.vue
@@ -2,10 +2,9 @@
   <div class="root">
     <div class="form-container">
       <form>
-        <input id="year" class="data-input" />年
-        <input id="month" class="data-input" />月
-        <input id="date" class="data-input" />日
-        <input id="show" type="button" value="表示" />
+        <input id="year" v-model.number="year" class="data-input" />年
+        <input id="month" v-model.number="month" class="data-input" />月
+        <input id="date" v-model.number="date" class="data-input" />日
       </form>
     </div>
     <div>
@@ -33,6 +32,15 @@
             土
           </th>
         </tr>
+        <tr v-for="week of weeks" :key="week.id">
+          <td
+            v-for="d of week.dates"
+            :key="d"
+            :class="{ targetDate: d === date }"
+          >
+            {{ d }}
+          </td>
+        </tr>
       </table>
     </div>
   </div>
@@ -40,7 +48,49 @@
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+import moment from 'moment'
+
+const today = moment()
+
+export default Vue.extend({
+  data: () => ({
+    year: today.year(),
+    month: today.month() + 1,
+    date: today.date()
+  }),
+  computed: {
+    weeks() {
+      const endDate = moment([this.year, this.month, this.date])
+        .endOf('month')
+        .date()
+      let day = today.startOf('month').day()
+      let week = {
+        id: 1,
+        startDay: day,
+        dates: new Array(day).fill(undefined)
+      }
+
+      const weeks = []
+      for (let date = 1; date <= endDate; date++) {
+        week.dates.push(date)
+        day++
+
+        if (day % 7 === 0) {
+          weeks.push(week)
+          day = 0
+          week = {
+            id: date + 1,
+            startDay: 0,
+            dates: []
+          }
+        }
+      }
+      weeks.push(week)
+
+      return weeks
+    }
+  }
+})
 </script>
 
 <style scoped>
@@ -57,5 +107,26 @@ export default Vue.extend({})
 input {
   border: solid 1px #000;
   margin: 0 10px;
+}
+
+th {
+  height: 50px;
+  width: 50px;
+  border-radius: 25px;
+  background-color: #303f9f;
+  color: #ffffff;
+}
+td {
+  height: 50px;
+  width: 50px;
+  border-radius: 25px;
+  background-color: #ffffff;
+  color: #000000;
+  text-align: center;
+}
+
+td.targetDate {
+  background-color: #303f9f;
+  color: #ffffff;
 }
 </style>
